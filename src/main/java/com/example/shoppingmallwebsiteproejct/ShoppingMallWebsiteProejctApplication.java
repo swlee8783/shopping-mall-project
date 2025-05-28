9a8b37c52dfb78c7d1b2d8e6a3a56cf7f626abc9
swlee8783 <siwon8783@gmail.com>
2025-05-28 23:58:32 +0900

    chore: initialize project with SpringBoot backend

---
+++ src/main/java/com/example/shoppingmallwebsiteproejct/ShoppingMallWebsiteProejctApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingmallwebsiteproejct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallWebsiteProejctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallWebsiteProejctApplication.class, args);
+    }
+
+}
